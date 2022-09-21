b2dfd78514171835d60fb3bee6f92177855f6437
Freedom1091 <1396747353@qq.com>
2022-09-21 17:27:58 +0800

    项目模板，自定义初始化完成

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="App">
+    <el-button>按钮</el-button>
+  </div>
+</template>
+
+<script>
+// 按需引入注册路由模块
+import { registerAPI } from '@/api'
+export default {
+  async created() {
+    const res = await registerAPI()
+    console.log(res)
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
